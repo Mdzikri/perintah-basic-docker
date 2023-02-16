9790310d5f7c2591e487f2ec5021d27c72fbfa00
dzikri huda <63085402+Mdzikri@users.noreply.github.com>
2023-02-16 19:13:33 +0700

    menambahkan masuk ke sql

--- docker-execution.sh
+++ docker-execution.sh
@@ -1,3 +1,6 @@
 #untuk masuk ke dalam container
 #-i interaktif -t sudo tty 
 docker container exec -i -t {nama container} /bin/bash
+
+#masuk ke mysql dengan adress
+mysql -h ipadress -u usernamedb -p 
