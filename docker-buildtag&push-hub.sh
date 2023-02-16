de32ce13ed2004d39209db9485da5e7dbbfc75d8
dzikri <dzikriftf@gmail.com>
2023-02-16 08:39:17 +0700

    tambah build,tag&push ke docker hub

---
+++ docker-buildtag&push-hub.sh
@@ -0,0 +1,10 @@
+
+#untuk build docker harus ada docker film yang dibuat sesuai dengan base image
+docker build -t {nama image yang akan dibuat}
+#setela build pastikan membuat kontainer dulu unutk dicoba 
+
+#tag adallah menggantin nama kontainer untuk di push ke hub docker 
+docker tag landing-page dzikrihuda/landingpage:v1.0
+
+
+docker push dzikrihuda/landingpage:v1.0 
