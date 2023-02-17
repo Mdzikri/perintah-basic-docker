fa0949554feef03074de243d311cd3a9587343a6
dzikri <dzikriftf@gmail.com>
2023-02-17 07:17:45 +0700

    menanmbahkan volume type bind

--- docker-volume.sh
+++ docker-volume.sh
@@ -3,3 +3,7 @@ docker volume create {nama volume}
 
 #ke backup data DB ke volume 
 docker run --name mysql -p 3306:3306 -v (volume):/var/lib/mysql -e MYSQL_ROOT_PASSWORD=dzikri -d mysql
+
+
+#cara menggunakan mount mysql
+docker run --name mongodata --publish 3306:3306 --mount "type=bind,source=(direktory),destination=/data/db" -e MYSQL_USER=aushafy -e MYSQL_ROOT_PASSWORD=dzikri mongo
