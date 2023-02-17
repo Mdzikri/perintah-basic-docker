7e368aa64297f225d0df8ffe6eb03c897f3dab78
dzikri huda <63085402+Mdzikri@users.noreply.github.com>
2023-02-18 06:40:48 +0700

    titik jangan lupa

--- docker-hub/docker-buildtag&push-hub.sh
+++ docker-hub/docker-buildtag&push-hub.sh
@@ -1,6 +1,6 @@
 
 #untuk build docker harus ada docker film yang dibuat sesuai dengan base image
-docker build -t {nama image yang akan dibuat}
+docker build -t {nama image yang akan dibuat} . 
 #setela build pastikan membuat kontainer dulu unutk dicoba 
 
 #tag adallah menggantin nama kontainer untuk di push ke hub docker 
