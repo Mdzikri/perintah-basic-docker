684696c939dbc60997440eb58233c854a4fe34d2
dzikri huda <63085402+Mdzikri@users.noreply.github.com>
2023-03-26 23:03:57 +0700

    Update docker-buildtag&push-hub.sh
    add docker login

--- docker-hub/docker-buildtag&push-hub.sh
+++ docker-hub/docker-buildtag&push-hub.sh
@@ -3,6 +3,9 @@
 docker build -t {nama image yang akan dibuat} . 
 #setela build pastikan membuat kontainer dulu unutk dicoba 
 
+#pastikan untuk connect ke docker hub nya dengan perintah 
+docker login
+
 #tag adallah menggantin nama kontainer untuk di push ke hub docker 
 docker tag landing-page dzikrihuda/landingpage:v1.0
 
